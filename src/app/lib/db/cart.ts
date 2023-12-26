732125d525538fab0ae88401658f39f4fce7dc84
Erastus Mauliate <emauliate17@gmail.com>
2023-12-26 18:39:29 +0700

    fix(nextjs-ecommerce): import prisma

--- src/app/lib/db/cart.ts
+++ src/app/lib/db/cart.ts
@@ -1,8 +1,8 @@
 import { cookies } from "next/dist/client/components/headers";
-import prisma from "./prisma";
 import { Cart, CartItem, Prisma } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { prisma } from "@/lib/db/prisma";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
   include: { items: { include: { product: true } } };
